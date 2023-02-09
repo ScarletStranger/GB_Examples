a22cf25cb777e2fa7db627c0a5fc88f97a21a4ab
ScarletStranger <maksimledenev6@gmail.com>
2023-02-09 11:27:41 +0300

    Sum of two ints

---
+++ Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Сумма введённых чисел равна " );
+Console.Write(numberA + numberB);
