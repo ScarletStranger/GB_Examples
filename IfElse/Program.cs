5208c7cb65141735e6e633a8067092885e375e08
ScarletStranger <maksimledenev6@gmail.com>
2023-02-09 11:56:13 +0300

    ifelse added

---
+++ IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "олеся")
+{
+    Console.WriteLine("Ура, это уже ОЛЕСЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
