67a8065b54f05eea1668ef80375189f6a0edde31
ScarletStranger <maksimledenev6@gmail.com>
2023-02-10 11:44:28 +0300

    First seminar

---
+++ Seminar1/Program.cs
@@ -0,0 +1,48 @@
+﻿//int n1 = -12;
+//double n2 = 12.12;
+//bool isEven = true;
+//string word = "car";
+//в char symbol = '' может храниться АБСОЛЮТНО ЛЮБОЙ символ
+
+//int n1 = -12,
+//    n2 = 6;
+
+//Console.WriteLine("My numbers are " + n1 + " and " + n2 + " and their sum is " + (n1 + n2));
+//Console.WriteLine($"My numbers are {n1} and {n2} and their sum is {n1 + n2}");
+
+//Console.Write("Input an integer number: ");
+//int n1 = Convert.ToInt32(Console.Read());
+//Console.WriteLine("Your number is " + n1);
+
+//Seminar1
+//Task 1. Напишите программу, которая на вход принимает два числа
+//и проверяет, является ли первое число квадратом второго.
+/*
+Console.Write("Input first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int power2 = n2 * n2;
+
+if(n1 == power2)
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
+*/
+
+//Task 2. Напишите программу, которая на вход принимает одно число (N),
+//а на входе показывает все целые числа и промежутке от -N до N.
+/*
+Console.Write("Input a positive number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+int curr = -n1;
+
+while(curr <= n1)
+{
+    Console.Write(curr + " ");
+    curr++;
+}
+*/
+
