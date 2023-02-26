72cb3ab7897c7cb7fc269f48b99691899730fc8a
ScarletStranger <maksimledenev6@gmail.com>
2023-02-26 23:09:02 +0300

    homework5 and fibon added

---
+++ Recurrent/Program.cs
@@ -0,0 +1,67 @@
+﻿/*string[,] table = new string[2, 5];//первое число(2) - строки, второе(5) - столбцы
+//вид данного массива:
+//столбцы - table [0,0] table[0,1] table [0,2] table [0,3] table [0,4] 
+//строки - table [1,0] 
+
+table[1, 2] = "слово";//первое число(1) - индекс строки, второе(2) - индекс столбца
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"+{table[rows, columns]}+");
+    }
+}
+*/
+
+/*void PrintArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            Console.Write($"{matr[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)
+        {
+            matr[rows, columns] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+*/
+
+/*double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 0; i < 15; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+*/
+
+int Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
---
+++ homework5/Program.cs
@@ -0,0 +1,124 @@
+﻿
+/*Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] RandomArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(100, 1000);
+    return array;
+}
+
+int Even(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    if(array[i] % 2 == 0)
+    count++;
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Input a number of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+
+int[] newArray = RandomArray(size);
+PrintArray(newArray);
+
+int total = Even(newArray);
+Console.WriteLine($"There are {total} even numbers in an array");
+*/
+
+/*Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] RandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(min, max + 1);
+    return array;
+}
+
+int OddSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i=i+2)
+        sum = array[i] + sum;
+    return sum;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Input a number of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a minimum value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a maximum value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = RandomArray(size, min, max);
+PrintArray(newArray);
+
+int total = OddSum(newArray);
+Console.WriteLine($"Sum of all odd numbers in an array eauals {total}");
+*/
+
+//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] RandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(min, max + 1);
+    return array;
+}
+
+int Difference(int[] array)
+{
+    int min = array[0];
+    int max = array[0];
+    int diff = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+        if (array[i] < min)
+            min = array[i];
+    }
+    return diff = max - min;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Input a number of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a minimum value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a maximum value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = RandomArray(size, min, max);
+PrintArray(newArray);
+
+int result = Difference(newArray);
+Console.WriteLine($"Difference between the smallest and the largest number is {result}");
