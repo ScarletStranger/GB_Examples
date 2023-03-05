ed4c22f0c46af67b9af4102a513ab8a78edcbf5d
ScarletStranger <maksimledenev6@gmail.com>
2023-03-05 13:04:07 +0300

    seminar7 added

---
+++ seminar7/Program.cs
@@ -0,0 +1,158 @@
+﻿/* 2D массив с рандомным размером и числами
+
+int[,] CreateRandom2DArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a minimum value: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a maximum value: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(min, max + 1);
+    return array;
+}
+
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] newArray = CreateRandom2DArray();
+Show2DArray(newArray);
+
+*/
+
+/* Каждый элемент массива находится по формуле a[i,j] = i+j
+
+int[,] CreateRandom2DArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = i + j;
+    return array;
+}
+
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] newArray = CreateRandom2DArray();
+Show2DArray(newArray);
+
+*/
+
+/* Возвести значение элементов с чётными индексами в квадрат
+
+int[,] CreateRandom2DArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a minimum value: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a maximum value: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(min, max + 1);
+    return array;
+}
+
+int[,] Square(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i += 2)
+        for (int j = 0; j < array.GetLength(1); j += 2)
+            array[i, j] *= array[i, j];
+    return array;
+}
+
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] newArray = CreateRandom2DArray();
+Show2DArray(newArray);
+Square(newArray);
+Show2DArray(newArray);
+
+*/
+
+/* Найти сумму элементов, находящихся на главной диагонали (0;0),(1;1) и т.д.
+
+int[,] CreateRandom2DArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a minimum value: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a maximum value: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(min, max + 1);
+    return array;
+}
+
+int GetSum(int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0) && i < array.GetLength(1); i++)
+        result += array[i, i];
+    return result;
+}
+
+void Show2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] newArray = CreateRandom2DArray();
+Show2DArray(newArray);
+Console.WriteLine(GetSum(newArray));
+
+*/
